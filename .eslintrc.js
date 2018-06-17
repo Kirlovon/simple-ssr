1514dd578858fa8a8c41f9bcff1f06efe6c1c434
Kirlovon <Kirlovon@outlook.com>
2018-06-17 16:34:33 +0300

    Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 2017
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-console": [
+			"off"
+		],
+		"quotes": [
+			"off"
+		],
+	}
+};
