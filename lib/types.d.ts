b3348887ab6250461a79567a50733db8b4f2563b
Kirlovon <Kirlovon@outlook.com>
2019-01-02 22:22:54 +0200

    Better types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,13 +1,29 @@
-/**	TypeScript types */
+/** TypeScript types */
 
 declare class simpleSSR {
+
+	/** Enable or Disable logs */
 	logs: boolean;
+
+	/** Safe mode for preventing errors and simplifying error messages */
 	safe: boolean;
+
+	/** Chromium browser from pupeteer.launch() */
 	browser: any;
+
+	/** To manually work with the cache */
 	cacheManager: any;
+
+	/** List of data types that are useless for rendering */
 	blockedRequest: string[];
-	start(config?: any): Promise<void>;
+
+	/** Start SimpleSSR and prepare SSR for work */
+	start(config?: object): Promise<void>;
+
+	/** Render specified URL */
 	render(url: string, config?: renderConfig): Promise<renderingResult>;
+
+	/** Stop SimpleSSR */
 	stop(): Promise<void>;
 }
 
